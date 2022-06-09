a5c714df65042722514fa1900251107a640678f5
Edward <edward.strudwick1@gmail.com>
2022-06-09 13:27:51 +0100

    Initial commit

---
+++ src/com/pomodoro/model/Attempt.java
@@ -0,0 +1,46 @@
+package com.pomodoro.model;
+
+public class Attempt {
+    private String mMessage;
+    private int mRemainingSeconds;
+    private AttemptKind mKind;
+
+    public Attempt(String message, AttemptKind kind) {
+        mMessage = message;
+        mKind = kind;
+        mRemainingSeconds = kind.getTotalSeconds();
+    }
+
+    public String getMessage() {
+        return mMessage;
+    }
+
+    public int getRemainingSeconds() {
+        return mRemainingSeconds;
+    }
+
+    public AttemptKind getKind() {
+        return mKind;
+    }
+
+    public void setMessage(String message) {
+        mMessage = message;
+    }
+
+    public void tick() {
+        mRemainingSeconds--;
+    }
+
+    @Override
+    public String toString() {
+        return "Attempt{" +
+                "mMessage='" + mMessage + '\'' +
+                ", mRemainingSeconds=" + mRemainingSeconds +
+                ", mKind=" + mKind +
+                '}';
+    }
+
+    public void save() {
+        System.out.printf("Saving: %s %n", this);
+    }
+}
---
+++ src/com/pomodoro/model/AttemptKind.java
@@ -0,0 +1,22 @@
+package com.pomodoro.model;
+
+public enum AttemptKind {
+    FOCUS (10, "Focus Time"),
+    BREAK (5, "Break Time");
+
+    private int mTotalSeconds;
+    private String mDisplayName;
+
+    AttemptKind(int totalSeconds, String displayName) {
+        mTotalSeconds = totalSeconds;
+        mDisplayName = displayName;
+    }
+
+    public int getTotalSeconds() {
+        return mTotalSeconds;
+    }
+
+    public String getDisplayName() {
+        return mDisplayName;
+    }
+}
